0273bf8f04396b8b738fc7b5d4633def287ce788
jmycsu <jiamingyi@csu.edu.cn>
2024-11-01 08:43:30 +0000

    修改运行脚本

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -4,14 +4,16 @@
 cd ../main/
 
 # 运行 main_EMR.py
-python main_EMR.py
+python main.py
 
 # 检查上一个命令是否成功执行
 if [ $? -eq 0 ]; then
     # 如果 main_EMR.py 成功执行，那么运行 evaluate.py
-    python evaluate.py
+    cd ../evaluate/
+    python process_output_emr.py
+    python evaluate_emr.py
 else
     # 如果 main_EMR.py 执行失败，那么打印错误消息并退出
-    echo "main_EMR.py failed, not running evaluate.py"
+    echo "main.py failed, not running evaluate.py"
     exit 1
 fi
